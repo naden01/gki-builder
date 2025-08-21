26c15524df423935044d8ea93bdedf75ad17731a
nazephyrus <126683488+naden01@users.noreply.github.com>
2025-08-21 11:46:54 +0700

    Change clang

--- config.sh
+++ config.sh
@@ -16,8 +16,8 @@ KERNEL_DEFCONFIG="gki_defconfig"
 # Release repository
 GKI_RELEASES_REPO="https://github.com/naden01/SuiKernel-releases"
 # Clang
-CLANG_URL="$(./clang.sh aosp)"
-CLANG_BRANCH=""
+CLANG_URL="https://github.com/mikeNG/android_prebuilts_clang_kernel_linux-x86_clang-r416183b"
+CLANG_BRANCH="lineage-20.0"
 # Zip name
 # Format: Kernel_name-Linux_version-Variant-Build_date
 ZIP_NAME="$KERNEL_NAME-KVER-VARIANT-BUILD_DATE.zip"
